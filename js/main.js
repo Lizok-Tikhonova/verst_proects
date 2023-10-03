09b87f09bb4df412b931786ef65366d7f6f98e13
Lizok-Tikhonova <tliza5092@gmail.com>
2023-10-03 21:41:34 +0300

    Темная тема + localStorage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const butNight = document.querySelector('.dark')
 
+
+if(localStorage.getItem('darkMode')=='dark'){
+    butNight.classList.add("dark--active")
+    document.body.classList.add("night")
+}
+
+
+
 butNight.addEventListener('click', ()=>{
     butNight.classList.toggle("dark--active")
+    const isDark = document.body.classList.toggle("night")
+
+    if(isDark){
+         localStorage.setItem('darkMode', 'dark')
+    }
+    else{
+        localStorage.setItem('darkMode', 'white')
+    }
+
+   
 })
